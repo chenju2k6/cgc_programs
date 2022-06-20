8320527f05969dce33503d1bfd2b13f1ed1925f1
Ju Chen <cju@bombur.cs.ucr.edu>
2022-06-20 10:28:17 -0700

    idjfkd
    Signed-off-by: Ju Chen <cju@bombur.cs.ucr.edu>

--- build.sh
+++ build.sh
@@ -22,8 +22,8 @@ echo "Creating Makefiles"
 CMAKE_OPTS="${CMAKE_OPTS} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
 # Honor CC and CXX environment variables, default to clang otherwise
-CC=${CC:-clang-12}
-CXX=${CXX:-clang++-12}
+CC=${CC:-gcc}
+CXX=${CXX:-g++}
 
 CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_C_COMPILER=$CC"
 CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_ASM_COMPILER=$CC"
